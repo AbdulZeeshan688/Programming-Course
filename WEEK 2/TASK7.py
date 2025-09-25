cfe0066420282c2642df6240e7e4c68f45a729f2
Abdul Zeeshan Mirza <mirzaabdulzeeshan890@gmail.com>
2025-09-25 09:09:34 +0300

    Add files via upload

---
+++ WEEK 2/TASK7.py
@@ -0,0 +1,10 @@
+#Program starting.
+print("program starting")
+
+#Insert fahrenheits: 50
+Fahrenheits = float(input("Insert temperature in Farenheit:  "))
+Calcious = (Fahrenheits -32)/1.8
+#50.0°F is 10.0°C
+print(f"{round(Fahrenheits,1)} °F  is {round(Calcious)}°C ")
+#Program ending.
+print("Program ending")
