72c225d178e10de8b46dadf92f1d96e7c6abf205
abdulzeeshan668 <your_email@example.com>
2025-12-10 08:02:07 +0200

    new tasks

--- WEEK 9/task4.py
+++ WEEK 9/task4.py
@@ -1,16 +1,27 @@
+import sys
+
+
 def readLines(PFilepath: str, PLines: list[str]) -> None:
-    Filehandle = open(PFilepath, 'r', encoding="UTF-8")
-    Line = Filehandle.readline()
-    while Line != '':
-        PLines.append(Line)
+    try:
+        Filehandle = open(PFilepath, 'r', encoding="UTF-8")
         Line = Filehandle.readline()
-    
+        while Line != '':
+            PLines.append(Line)
+            Line = Filehandle.readline()
+    #except FileNotFoundError:
+        #print("try another file.")
+        #lets start the progeam from the begining
+    except Exception:
+        print("File found, but something else went wrong.")
+        sys.exit(1)
     return None
 
 def main() -> None:
     Lines: list[str] = []
     Filename = input("Insert filename: ")
     readLines(Filename, Lines)
+    for Line in Lines:
+        print(Line)
     return None
 
 main()
